cff36727f48bf41eb17807dbe5d5d057c4500d27
icaboalo <icaboalo@gmail.com>
2015-09-23 18:43:27 -0500

    Custom Toolbar

--- app/src/main/java/com/icaboalo/amazonapilist/MainActivity.java
+++ app/src/main/java/com/icaboalo/amazonapilist/MainActivity.java
@@ -1,9 +1,10 @@
 package com.icaboalo.amazonapilist;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,17 +21,20 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     List<Course> myCourseList;
     RecyclerViewAdapter recyclerViewAdapter;
+    Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
 
         setUpRecyclerView();
     }
